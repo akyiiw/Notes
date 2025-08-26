814b2f6be10dfc379dc6cf603f0090b885f32388
Pietro <itachinhorp@gmail.com>
2025-08-25 21:13:25 -0300

    pequenas mudanças
    QOL changes

---
+++ Notes/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace Notes;
+
+public partial class AboutPage : ContentPage
+{
+    public AboutPage()
+    {
+        InitializeComponent();
+    }
+
+    private void EnviarGitBtn_Clicked(object sender, EventArgs e)
+    {
+        Launcher.Default.OpenAsync("https://github.com/Jolimenezes");
+    }
+}
---
+++ Notes/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿namespace Notes
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string caminho = Path.Combine(FileSystem.AppDataDirectory,"arquivo");
+
+        public MainPage()
+        {
+            InitializeComponent();
+            if (File.Exists(caminho))
+                Edit.Text = File.ReadAllText(caminho);
+
+        }
+       
+        private void Edit_TextChanged(object sender, TextChangedEventArgs e)
+        {
+ 
+        }
+
+        private void Edit_Completed(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SalvarBtn_Clicked(object sender, EventArgs e)
+        {
+            string conteudo = Edit.Text;
+            File.WriteAllText(caminho, conteudo);
+            DisplayAlert($"O arquivo de nome {File.GetAttributes(caminho)} foi salvo!", $"O arquivo foi salvo com sucesso em {caminho}", "Ok");
+
+        }
+
+        private void ApagarBtn_Clicked(object sender, EventArgs e)
+        {
+            if (File.Exists(caminho))
+            {
+                File.Delete(caminho);
+                DisplayAlert("Arquivo apagado", "Arquivo apagado com sucesso", "Ok");
+                Edit.Text = null;
+            } 
+           else
+           {
+                DisplayAlert("O arquivo não foi encontrado", "Certifique-se de ter criado um arquivo antes de tentar excluir um", "ok");
+           }
+    }
+    }
+
+}
