7ea2614668ee640d82461be0d33651913b3a865c
Pietro <itachinhorp@gmail.com>
2025-09-30 20:20:21 -0300

    Last commit
    cache mods

--- Notes/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Notes.AssemblyInfo.cs
+++ Notes/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Notes.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+814b2f6be10dfc379dc6cf603f0090b885f32388")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+55650552b9b57e8071e5cef43140fec7b6d015fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
